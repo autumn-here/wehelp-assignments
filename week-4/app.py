8235b35184470b09a154f91cc65ab3a0b16e7b73
autumn-here <95583422+autumn-here@users.noreply.github.com>
2022-01-23 20:41:32 +0800

    modify verify, not to use global variable

--- week-4/app.py
+++ week-4/app.py
@@ -1,47 +1,23 @@
-from flask import Flask
-from flask import request
-from flask import redirect 
-from flask import url_for
-from flask import render_template
-from flask import session
+from flask import Flask, request, redirect, url_for, render_template, session
 import os
 app = Flask(__name__)
 app.secret_key = os.urandom(20)
 
-#建立網站首頁的回應方式
-#login page
-# @app.route("/", methods=["POST","GET"])
-# def login():
-#     if request.method == "POST":
-#         user = request.form["nm"]
-#         return redirect(url_for("user",usr=user))
-#     else:
-#         return render_template("home.html")
-#user page
-# @app.route("/<usr>")
-# def user(user):
-#     return render_template("success.html",user=user)
-# if __name__ =="__main__":
-#     app.run(debug=True)
-
-
 @app.route("/", methods=["POST","GET"])
 def login():
-  global user, password
   if request.method == "POST":
-    user = request.form["username"]
-    password = request.form["password"]
     return redirect(url_for("verify"))
   else:
     return render_template('home.html')
 
-@app.route("/signin/")
+@app.route("/signin/", methods=["POST"])
 def verify():
+  user = request.form["username"]
+  password = request.form["password"]
   if user == "test" and password == "test":
     session["user"] = user
     return redirect(url_for("member"))
   elif user == "" or password == "":
-    global message
     message = "請輸入帳號、密碼"
     return redirect(url_for('error', message=message))
   elif user != "test" or password != "test":
